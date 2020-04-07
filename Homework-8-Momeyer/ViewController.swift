baa5c9a1a0aa4f3e53c4df3b0430f55c6bc39c37
Trevor Momeyer <t.momeyer@setonhill.edu>
2020-04-07 12:37:31 -0400

    Initial Commit

---
+++ Homework-8-Momeyer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework-8-Momeyer
+//
+//  Created by Trevor Momeyer on 4/7/20.
+//  Copyright © 2020 Trevor Momeyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
